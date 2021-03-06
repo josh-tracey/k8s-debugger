f2ac20e30854ed36f21e5e14e5df0b99093e56a2
Joshua Tracey <joshua.tracey08@gmail.com>
2021-03-06 11:31:52 +1000

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 #!node
 import 'reflect-metadata'
 import { mainMenu } from './mainMenu'
-// import { showPodDetails } from './api/k8s/resources'
 import ContextSwitcher from './operations/contextSwitch'
 import NamespaceSwitcher from './operations/namespaceSwitcher'
 
