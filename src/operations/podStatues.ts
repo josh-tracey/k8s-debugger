fd02483a8b3da87849456a74b318e2a93bef8549
Joshua Tracey <joshua.tracey08@gmail.com>
2021-04-12 08:47:31 +1000

    fix up pod status

--- src/operations/podStatues.ts
+++ src/operations/podStatues.ts
@@ -2,22 +2,17 @@ import api from '../api'
 import RootStore from '../store'
 import * as columnify from 'columnify'
 import { IOperation } from './interface'
-
+import { colorStatus } from '../helpers'
 
 //TODO Needs to be updated, not too great, doesn't reflect the current state correctly.
 const PodStatuses: IOperation = {
   execute: async () => {
-    const data: any[] = []
-    const pods = (await api.getPods(RootStore.currentNamespace)).body.items.map(
-      (pod) => pod.metadata?.name!
-    )
-    await Promise.all(
-      pods.map(async (pod: string) => {
-        if (pod) {
-          data.push(await api.getPodStatus(pod!, RootStore.currentNamespace!))
-        }
-      })
-    )
+    const data = (await api.getPods(RootStore.currentNamespace)).body.items
+      .map((pod) => ({
+        Name: pod.metadata?.name,
+        Status: colorStatus(pod.status?.phase),
+      }))
+      .sort((a, b) => (a.Name! <= b?.Name! ? -1 : 1))
     const columns = columnify(data, {
       minWidth: 20,
       config: { name: { maxWidth: 30 } },
